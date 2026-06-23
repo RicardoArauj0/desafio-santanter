d2de697b8f3d7a1b817b75cf1a997bfa71935f37
Ricardo Araujo <ricardofranc.araujo@gmail.com>
2026-06-23 11:41:02 -0300

    Delete CEPApiClient.java

--- CEPApiClient.java
+++
@@ -1,9 +0,0 @@
-package com.buscacep.santander.service;
-
-import com.buscacep.santander.dto.BuscaCEPRespostaAPIDTO;
-
-public interface CEPApiClient {
-
-    BuscaCEPRespostaAPIDTO buscarCep(String cep);
-
-}
